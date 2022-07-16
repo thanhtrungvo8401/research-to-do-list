b33ed9aba077f50ed009c3da9d866de6d5d40f1f
trungvt <thanhtrungvo8401@gmail.com>
2022-07-16 16:16:39 +0700

    FE upload file

--- public/client-upload.js
+++ public/client-upload.js
@@ -1,5 +1,26 @@
 function submit(e) {
     e?.preventDefault();
 
-    console.log("SUBMIT")
+    const fileEl = document.querySelector("input[name='file']")
+    const nameEl = document.querySelector("input[name='name']")
+
+    fetch('/upload', 
+        { method: 'POST', body: { file: fileEl[0], name: nameEl.value } }
+    ).then(res => {
+        return res.json()
+    })
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
 }
+
+
+window.addEventListener('load', function () {
+    const form = document.getElementById('form')
+
+    form.addEventListener('submit', submit)
+})
--- sevices/routes.js
+++ sevices/routes.js
@@ -6,4 +6,10 @@ module.exports = function (app) {
     app.get('/upload', (req, res) => {
         res.render('upload', { title: 'Upload file' })
     })
+
+    app.post('/upload', (req, res) => {
+        console.log('UPLOAD FILE', req)
+
+        res.json({ success: true })
+    })
 }
