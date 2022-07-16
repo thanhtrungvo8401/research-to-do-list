ae6979f94cbd54e0709058b3108013865dc46c1f
trungvt <thanhtrungvo8401@gmail.com>
2022-07-16 14:16:07 +0700

    register routes

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const cron = require('./sevices/cron')
-const app = express()
+const http = require('http');
+const routesRegister = require('./sevices/routes');
+
 const port = 3000
+const app = express()
+const server = http.createServer(app);
 
 // init services:
 cron();
 
+// register root:
+routesRegister(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
 })
